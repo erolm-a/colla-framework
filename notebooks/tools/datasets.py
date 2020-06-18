b3888bcb509b6e3d55c7bb78db85f5a7f717d2ce
Enrico "erolm_a" Trombetta <trombetta.enricom@protonmail.com>
2020-06-18 13:08:38 +0000

    Added annotation queries

--- notebooks/tools/datasets.py
+++ notebooks/tools/datasets.py
@@ -43,3 +43,68 @@ def get_wikidata_link(entity, _format="json", flavor=None):
     if flavor is not None:
         url += f"?flavor={flavor}"
     return url
+
+
+def wikidata_get_edges(entity_df):
+    """Generate triples: [<subject>, <predicate>, <object>].
+        Only consider objects for which there is a linked entity"""
+    
+    root = entity_df["entities"]
+    subjects = list(entity_df["entities"].keys())
+    
+    predicates = [(subject, claim, root[subject]["claims"][claim]) for subject in subjects for claim in root[subject]["claims"]]
+
+    tuples = []
+    
+    for (subject, claim, snaks) in predicates:
+        for snak in snaks:
+            mainsnak = snak["mainsnak"]
+            if mainsnak["snaktype"] == "value" and mainsnak["datavalue"]["type"] == "wikibase-entityid":
+                tuples.append((subject, claim, mainsnak["datavalue"]["value"]["id"]))
+            
+    
+    return tuples
+
+
+def sparql_values_in(l, prefix="wd:"):
+    return " ".join([prefix + entity for entity in l])
+
+
+def annotate_entity(entity_list):
+    query_list = sparql_values_in(entity_list)
+    
+    print(query_list)
+    
+    # multi-line f-strings seem to be broken with this version of jupyter.
+    query = """
+    SELECT ?entity ?label
+    WHERE
+    {
+        VALUES ?entity {query_list}.
+        ?entity rdfs:label ?label.
+        FILTER(LANG(?label) = "en").
+    }"""
+    
+    query = query.replace("query_list", query_list)
+    wikidata_sparql.setQuery(query)
+    results =  wikidata_sparql.query().convert()
+    return pd.json_normalize(results['results']['bindings'])
+    
+
+def annotate_property(property_list):
+    query_list = sparql_values_in(property_list)
+    
+    query = """
+    SELECT ?property ?propertyLabel WHERE {
+        ?property a wikibase:Property .
+        VALUES ?property {?property_list}
+        SERVICE wikibase:label {
+            bd:serviceParam wikibase:language "en" .
+        }
+    }
+    """
+    
+    query = query.replace("?property_list", query_list)
+    wikidata_sparql.setQuery(query)
+    results =  wikidata_sparql.query().convert()
+    return pd.json_normalize(results['results']['bindings'])
