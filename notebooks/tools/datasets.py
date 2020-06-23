76d3d428ecda1d632da6b2e8fa17da503d444e6c
Enrico "erolm_a" Trombetta <trombetta.enricom@protonmail.com>
2020-06-23 16:04:51 +0000

    Improved extraction pipeline

--- notebooks/tools/datasets.py
+++ notebooks/tools/datasets.py
@@ -3,13 +3,13 @@ Some tools to cache datasets and query them
 '''
 
 import os
+import json
 import requests
+import pandas as pd
 
-from SPARQLWrapper import SPARQLWrapper, JSON
+from .sparql_wrapper import dbpedia_sparql, wikidata_sparql
 
 DATA_FOLDER = "data"
-wikidata_sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
-wikidata_sparql.setReturnFormat(JSON)
 
 
 def get_filename_path(filename):
@@ -23,26 +23,95 @@ def wrap_open(filename, *args, **kwargs):
     return open(path, *args, **kwargs)
 
 
-def fetch_dataset(url, filename):
+def download_to(url: str, filename: str):
+    r = requests.get(url)
+    with wrap_open(filename, "wb") as f:
+        f.write(r.content)
+
+        
+def fetch_wikidata_dataset(entity, _format="json", flavor=None):
+    url = get_wikidata_link(entity, _format=_format, flavor=flavor)
+    filename = entity + "." + _format
+    
     if os.path.isfile(get_filename_path(filename)):
-        print(f"File {filename} was already downloaded. Skipping...")
+        print(f"Dataset {filename} already downloaded. Skipping...")
     else:
-        print(f"Dataset not available, downloading from {url}")
+        print(f"Dataset {filename} not available, downloading from {url}")
         r = requests.get(url)
         with wrap_open(filename, "wb") as f:
             f.write(r.content)
         print("Done")
 
 
-def get_wikidata_link(entity, _format="json", flavor=None):
+def get_wikidata_link(entity, _format="json", flavor=None) -> str:
     """
-    Extract a wikidata entity. Very simple and crude. More sofisticated queries
-    should use a SPARQLWrapper.
+    Extract a wikidata entity link. Very simple and crude. More sofisticated
+    queries should use a SPARQLWrapper.
     """
+    
     url = f"https://wikidata.org/wiki/Special:EntityData/{entity}.{_format}"
     if flavor is not None:
         url += f"?flavor={flavor}"
     return url
+
+def get_dbpedia_link(entity, _format="json") -> str:
+    """Extract a dbpedia entity. Very simple and crude. More sofisticated
+    queries should use a SPARQLWrapper.
+    
+    Note that in DBPedia wikipedia links are entity identifiers and are
+    case-sensitive. The URL fetcher seems *not* to be solving the redirects
+    alone.
+    """
+    return f"https://dbpedia.org/data/{entity}.{_format}"
+    
+
+def fetch_dbpedia_dataset(name: str, _format="json", force_redownload=False):
+    """Download and cache a dbpedia dataset.
+    
+    Note that in DBPedia wikipedia links are entity identifiers and are
+    case-sensitive. The URL fetcher seems *not* to be solving the redirects alone,
+    but it is not guaranteed to always be the case. For such reasons, please
+    avoid using a SPARQL's "DESCRIBE" query to dump an entity."""
+    filename = name + "." + _format
+    url = get_dbpedia_link(name, _format)
+    
+    if os.path.isfile(get_filename_path(name) and not force_redownload):
+        print(f"Dataset {filename} already downloaded. Skipping...")
+    else:
+        print(f"Dataset {filename} already available. Downloading from {url}")
+        download_to(url, filename)
+        print("Done")
+        
+
+def fetch_dataset(entity:str, provider:str, flavor : str = None, force_redownload : bool = False):
+    """Fetch a dataset. This function abstracts out the mechanics of
+    dataset harvesting and dumping.
+    
+    Accepted values for provider:
+    
+    - "wikidata"
+    - "dbpedia"
+    
+    Accepted values for flavor (supported by Wikidata provider only):
+    
+    - None (default): extract all data (including metadata and qualifiers)
+    - simple: extract all tuples minus metadata and qualifiers
+    """
+    
+    if provider == "wikidata":
+        fetch_wikidata_dataset(entity, flavor=flavor)
+        # TODO: possibly support other serialization options?
+        with wrap_open(entity + ".json") as fp:
+            return json.load(fp)
+    
+    elif provider == "dbpedia":
+        fetch_dbpedia_dataset(entity, force_redownload=force_redownload)
+        
+        with wrap_open(entity + ".json") as fp:
+            return json.load(fp)
+
+    else:
+        raise Exception(f"Unsupported provider: {provider}")
 
 
 def wikidata_get_edges(entity_df):
@@ -65,17 +134,15 @@ def wikidata_get_edges(entity_df):
     
     return tuples
 
-
 def sparql_values_in(l, prefix="wd:"):
     return " ".join([prefix + entity for entity in l])
 
 
-def annotate_entity(entity_list):
+def annotate_wikidata_entity(entity_list):
     query_list = sparql_values_in(entity_list)
     
     print(query_list)
     
-    # multi-line f-strings seem to be broken with this version of jupyter.
     query = """
     SELECT ?entity ?label
     WHERE
@@ -86,12 +153,12 @@ def annotate_entity(entity_list):
     }"""
     
     query = query.replace("query_list", query_list)
-    wikidata_sparql.setQuery(query)
-    results =  wikidata_sparql.query().convert()
-    return pd.json_normalize(results['results']['bindings'])
+    return wikidata_sparql.run_query(query)
     
 
-def annotate_property(property_list):
+def annotate_wikidata_property(property_list):
+    """Fetch the property name of a list of properties"""
+    
     query_list = sparql_values_in(property_list)
     
     query = """
@@ -105,6 +172,26 @@ def annotate_property(property_list):
     """
     
     query = query.replace("?property_list", query_list)
-    wikidata_sparql.setQuery(query)
-    results =  wikidata_sparql.query().convert()
-    return pd.json_normalize(results['results']['bindings'])
+    return wikidata_sparql.run_query(query)
+
+
+def get_wikidata_edges(entity_df: dict):
+    """Generate triples: [<subject>, <predicate>, <object>].
+        Only consider objects for which there is a linked entity"""
+    
+    root = entity_df["entities"]
+    subjects = list(entity_df["entities"].keys())
+    
+    predicates = [(subject, claim, root[subject]["claims"][claim]) for subject in subjects for claim in root[subject]["claims"]]
+
+    tuples = []
+    
+    for (subject, claim, snaks) in predicates:
+        for snak in snaks:
+            mainsnak = snak["mainsnak"]
+            if mainsnak["snaktype"] == "value" and \
+               mainsnak["datavalue"]["type"] == "wikibase-entityid":
+                tuples.append((subject, claim, mainsnak["datavalue"]["value"]["id"]))
+            
+    
+    return tuples
---
+++ notebooks/tools/sparql_wrapper.py
@@ -0,0 +1,54 @@
+"""Some data sources (DBPedia, Wikidata) provide a SPARQL endpoint. However, their ontologies differ radically.
+
+Here we provide some tools to simplify queries by providing proper prefixes."""
+
+import pandas as pd
+from SPARQLWrapper import SPARQLWrapper, JSON, JSONLD
+
+class DBPediaQuery:
+    """Wrapper to make SPARQL queries to DBPedia"""
+    def __init__(self):
+        self.sparql = SPARQLWrapper("http://dbpedia.org/sparql/")
+        self.sparql.setReturnFormat(JSON)
+        
+    def gen_query(self, query):
+        """Add proper prefixes for the ontologies."""
+        
+        return """
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX dc: <http://purl.org/dc/elements/1.1/>
+PREFIX : <http://dbpedia.org/resource/>
+PREFIX dbpedia2: <http://dbpedia.org/property/>
+PREFIX dbpedia: <http://dbpedia.org/>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+""" + query
+    
+    def run_query(self, query):
+        generated = self.gen_query(query)
+        self.sparql.setQuery(generated)
+        results =  self.sparql.query().convert()
+        return pd.json_normalize(results['results']['bindings'])
+
+
+class WikidataQuery:
+    """Wrapper to make SPARQL queries to Wikidata"""
+    def __init__(self):
+        self.sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+        self.sparql.setReturnFormat(JSON)
+        
+    def gen_query(self, query):
+        return query
+    
+    def run_query(self, query):
+        generated = self.gen_query(query)
+        self.sparql.setQuery(generated)
+        results =  self.sparql.query().convert()
+        return pd.json_normalize(results['results']['bindings'])
+
+# Export those two globally
+dbpedia_sparql = DBPediaQuery()
+wikidata_sparql = WikidataQuery()
