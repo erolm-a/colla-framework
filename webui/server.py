60b39f1573b4fd1785c18b76e5613dcdaa10f85b
Enrico "erolm_a" Trombetta <trombetta.enricom@protonmail.com>
2020-07-07 09:44:47 +0100

    flask skeleton

---
+++ webui/server.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_restful_swagger import swagger
+
+app = Flask(__name__)
+api = swagger.docs(Api(app), apiVersion='0.1')
+
+class HelloWorld(Resource):
+    def get(self):
+        return {"hello": "world"}
+
+api.add_resource(HelloWorld, "/")
+
+if __name__ == '__main__':
+    app.run(debug=True)
