2b6730e82bfbbaf7db251c5251a08078070da15c
Enrico "erolm_a" Trombetta <trombetta.enricom@protonmail.com>
2020-07-07 15:11:53 +0000

    Flask can serve Vue

--- webui/backend/app.py
+++ webui/backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_file
 from flask_restful import Resource, Api
 from flask_restful_swagger import swagger
 
@@ -9,7 +9,13 @@ class HelloWorld(Resource):
     def get(self):
         return {"hello": "world"}
 
+
+
 api.add_resource(HelloWorld, "/api")
+
+@app.route("/")
+def serve_frontend():
+    return send_file('build/index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
