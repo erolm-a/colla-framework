e702fd795dfc06b1a52c42ed8f448abe9d815588
Enrico "erolm_a" Trombetta <trombetta.enricom@protonmail.com>
2020-07-08 16:36:17 +0100

    Chat component skeleton

--- webui/frontend/src/App.vue
+++ webui/frontend/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
+      <router-link to="/chat">Chat</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <b-container fluid>
---
+++ webui/frontend/src/views/Chat.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="chat">
+    <ChatContainer/>
+  </div>
+</template>
+
+<script>
+import ChatContainer from '@/components/ChatContainer';
+
+export default {
+  name: 'Home',
+  components: {
+    ChatContainer,
+  },
+};
+</script>
