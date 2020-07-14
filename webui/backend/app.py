82468eceac7dbf52665f35ada08611c34944cb5e
Enrico "erolm_a" Trombetta <trombetta.enricom@protonmail.com>
2020-07-14 14:13:06 +0000

    Improved intent classification, bugfixes

--- webui/backend/app.py
+++ webui/backend/app.py
@@ -1,58 +1,73 @@
 from flask import Flask, send_file, redirect, session
-from flask_restful import Resource, Api, reqparse
+from flask_restful import Resource, Api, reqparse, fields, marshal_with
 from flask_restful_swagger import swagger
+import pickle
 
-from tools.answering import answer_natural_question, QuestionAnsweringContext
+from tools.answering import QuestionAnsweringContext, SerializedIntent, failed_intent
 
 app = Flask(__name__)
-api = swagger.docs(Api(app), apiVersion='0.1')
+api = swagger.docs(Api(app),
+                   apiVersion='0.1',
+                   description="A Basic Chatbot API")
 
 app.secret_key = b'\x97\xba\xbb\xe4\xac5\x94\x10c\xda\x82\xb0\x9d\xe9\xa2|'
 
-class HelloWorld(Resource):
-    def get(self):
-        return {"hello": "world"}
+@swagger.model
+class IntentResponse:
+    resource_fields = {
+        'intentType': fields.String,
+        'message': fields.String
+    }
 
+@swagger.model
+class Utterance:
+    resource_fields = {
+        'utterance': fields.String
+    }
 
-class QuestionAnsweringResource(Resource):
-    @swagger.operation(
-        notes="""Answer a natural question. This call is not meant to be used
-        in a dialogue-like situation where previous context is needed."""
-    )
-    def get(self, natural_question):
-        """Answer context-less natural language questions."""
-
-        senses = answer_natural_question(natural_question)
-        return {"intentType": "expressionDefinition",
-                "senses": senses
-                }
-        
 class DialogueAnsweringResource(Resource):
     @swagger.operation(
         notes="""Create a session. The call leaves a FLASK_SESSION
-        token that expires after 24 hours, or explicitely deleted."""
+        token that expires after 24 hours, or explicitely deleted.""",
+        responseClass=IntentResponse.__name__
     )
+    @marshal_with(IntentResponse.resource_fields)
     def get(self):
-        session['context'] = QuestionAnsweringContext()
-        return {'welcomeText': 'Hi, this is the Grill-Lab chatbot! This context will terminate in 24 hours.'}
+        session['context'] = pickle.dumps(QuestionAnsweringContext())
+        return SerializedIntent(SerializedIntent.IntentType.WELCOME, 'Hi, this is the Grill-Lab chatbot! This context will terminate in 24 hours.')
     
     @swagger.operation(
-        notes="Reply to an user utterance."
+        notes="Reply to an user utterance.",
+        responseClass=IntentResponse.__name__,
+        parameters=[
+            {
+                "name": "utterance",
+                "description": "The user utterance",
+                "required": True,
+                "allowMultiple": False,
+                "dataType": Utterance.__name__,
+                "paramType": "body"
+            }
+        ],
+
     )
+    @marshal_with(IntentResponse.resource_fields)
     def post(self):
         if not 'context' in session:
-            return {'response': 'Context was not created.'}, 401
+            return failed_intent('Context was not created')
 
-        context = session['context']
+        context = pickle.loads(session['context'])
 
         argparser = reqparse.RequestParser()
         argparser.add_argument('utterance')
         args = argparser.parse_args()
 
-        return context.handle_question(args['utterance'])
+        response = context.handle_question(args['utterance'])
+        session['context'] = pickle.dumps(context)
+
+        return response
+
         
-api.add_resource(HelloWorld, "/api")
-api.add_resource(QuestionAnsweringResource, "/api/query/<string:natural_question>")
 api.add_resource(DialogueAnsweringResource, "/api/chat")
 
 @app.route("/")
