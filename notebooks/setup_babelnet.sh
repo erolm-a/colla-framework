480f61da671cb7e4ffbb5c0232d503cdfc1dcb31
Enrico "erolm_a" Trombetta <trombetta.enricom@protonmail.com>
2020-08-20 03:40:52 +0100

    yet another fix

--- notebooks/setup_babelnet.sh
+++ notebooks/setup_babelnet.sh
@@ -8,12 +8,11 @@ DOWNLOAD_DESTINATION=/tmp/$BABELNET_FILENAME
 UNZIP_PATH=/tmp/$(basename -s .zip $DOWNLOAD_DESTINATION)
 
 LIB_FOLDER="3rdparty/lucene-index"
-mkdir -p $LIB_FOLDER/lib
+mkdir -p $LIB_FOLDER
 
 wget $BABELNET_URL -O $DOWNLOAD_DESTINATION
 unzip $DOWNLOAD_DESTINATION -d /tmp
-
-cp $UNZIP_PATH/*.jar $LIB_FOLDER
-cp -r $UNZIP_PATH/lib $LIB_FOLDER/lib
+cp -r $UNZIP_PATH/*.jar $LIB_FOLDER/
+cp -r $UNZIP_PATH/lib/*.jar $LIB_FOLDER/lib
 cp -r $UNZIP_PATH/config config
 cp -r $UNZIP_PATH/resources resources
